6fa6c78ec5b19b7bea1dd7faf1e77828402464bb
Mateus Cardoso <mateus7.cardoso@hotmail.com>
2026-05-18 15:25:53 -0300

    feat: add stored procedure to get all users

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -24,3 +24,46 @@ select * from cargo;
 call reajuste_salario(10, 'gerente');
 
 select * from cargo;
+
+
+
+
+create or replace procedure sorteio_cliente()
+language plpgsql
+as
+$$
+declare
+    cliente_sorteado integer;
+    nome_cliente varchar(100);
+    cliente_especial_existe integer;
+begin
+
+    select id, nome
+    into cliente_sorteado, nome_cliente
+    from cliente
+    order by random()
+    limit 1;
+
+    select count(*)
+    into cliente_especial_existe
+    from cliente_especial
+    where cliente_id = cliente_sorteado;
+
+    if cliente_especial_existe > 0 then
+
+        raise notice
+        'cliente % ganhou voucher de r$200',
+        nome_cliente;
+
+    else
+
+        raise notice
+        'cliente % ganhou voucher de r$100',
+        nome_cliente;
+
+    end if;
+
+end;
+$$;
+
+call sorteio_cliente();
